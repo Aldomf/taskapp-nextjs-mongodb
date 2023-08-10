07fa551ca51554e7bde2ca36a936078fdaf81dce
Aldo Miralles <alditomiralles3@gmail.com>
2023-08-10 17:21:32 +0200

    adding env file

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,4 +1,8 @@
 import { connect, connection } from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const passwordDB = process.env.PASSWORD_DB
 
 const conn = {
   isConnected: false,
@@ -8,7 +12,7 @@ export async function connectDB() {
   if (conn.isConnected) return;
 
   const db = await connect(
-    `mongodb+srv://alditomiralles3:alditomongoDB008@aldo.l3up4qv.mongodb.net/tasks?retryWrites=true&w=majority`
+    `mongodb+srv://alditomiralles3:${passwordDB}@aldo.l3up4qv.mongodb.net/tasks?retryWrites=true&w=majority`
   );
   console.log(db.connection.db.databaseName);
   conn.isConnected = db.connections[0].readyState;
