189c0416888d9501075504ef379ce7ca3c66c594
Aldo Miralles <alditomiralles3@gmail.com>
2023-08-17 13:40:38 +0200

    updating middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 export {default} from "next-auth/middleware"
 
 export const config = {
-    matcher: ["/dashboard/:path*", "/", "/tasks/new", "/signout"]
+    matcher: ["/dashboard/", "/tasks/new", "/signout"]
 }
